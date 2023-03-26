92a49763dcc7500c06b15a730527edc9380c831b
voroshkov <voroshkov@gmail.com>
2023-03-26 19:29:49 +0300

    Task #2 Serve SPA in AWS - use unique bucket name in serverless.yml - change shop name to test invalidation in AWS CloudFront - add links to S3 and CloudFront to Readme

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/" underline="hover">
-        My Store
+        AV AWS Learn Store
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
